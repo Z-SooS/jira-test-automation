100f2564dd3476e6971aa02e00608c118aae16d0
Z-SooS <zoli.soos98@gmail.com>
2022-09-06 12:24:42 +0200

    Added parameterized test for browseConfiguredProjects

--- src/test/java/JIRA/BrowseProjects.java
+++ src/test/java/JIRA/BrowseProjects.java
@@ -2,6 +2,9 @@ package JIRA;
 
 import Main.FileReader;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,6 +13,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BrowseProjects {
     private WebDriver webDriver;
@@ -63,5 +67,25 @@ public class BrowseProjects {
         String main = webDriver.findElement(By.id("main")).getText();
 
         Assertions.assertTrue(main.contains(expectedError));
+    }
+
+    @ParameterizedTest
+    @MethodSource("projects")
+    public void browseConfiguredProjects(String url, String key)
+    {
+        user.login();
+
+        webDriver.navigate().to(url);
+
+        String projectMeta = webDriver.findElement(By.className("project-meta")).getText();
+
+        Assertions.assertTrue(projectMeta.contains(key));
+    }
+    private static Stream<Arguments> projects() {
+        return Stream.of(
+                Arguments.arguments("https://jira-auto.codecool.metastage.net/projects/TOUCAN/summary","TOUCAN"),
+                Arguments.arguments("https://jira-auto.codecool.metastage.net/projects/JETI/summary", "JETI"),
+                Arguments.arguments("https://jira-auto.codecool.metastage.net/projects/COALA/summary", "COALA")
+        );
     }
 }
