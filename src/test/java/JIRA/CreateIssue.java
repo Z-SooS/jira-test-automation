6b7db67129eaa3b9e22db72b470b70a11a28d039
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2022-09-06 13:12:46 +0200

    createIssue started implementing

--- src/test/java/JIRA/CreateIssue.java
+++ src/test/java/JIRA/CreateIssue.java
@@ -1,4 +1,56 @@
 package JIRA;
 
+import Main.FileReader;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class CreateIssue {
+    private WebDriver webDriver;
+    private User user;
+    private static ChromeOptions browserOptions;
+
+    @BeforeAll
+    public static void setProperty(){
+        System.setProperty("webdriver.chrome.driver", FileReader.getValueByKey("driver.location"));
+
+        browserOptions = new ChromeOptions();
+        browserOptions.addArguments("--incognito");
+    }
+
+    @BeforeEach
+    public void openNewTab(){
+        webDriver = new ChromeDriver(browserOptions);
+        user = new User(webDriver);
+    }
+
+    @AfterEach
+    public void closeTab(){
+        webDriver.close();
+    }
+
+    @Test
+    public void createIssue(){
+        // TODO not working yet
+        user.login();
+
+        webDriver.get("https://jira-auto.codecool.metastage.net/browse/MTP-749?filter=-5");
+        webDriver.findElement(By.id("create_link")).click();
+        RandomHelper.Wait(webDriver);
+
+        webDriver.findElement(By.id("project-field")).clear();
+        webDriver.findElement(By.id("project-field")).sendKeys("Main Testing Project (MTP)");
+
+        webDriver.findElement(By.id("issuetype-field")).clear();
+        webDriver.findElement(By.id("issuetype-field")).sendKeys("Bug");
+
+        webDriver.findElement(By.id("summary")).sendKeys("New bug issue");
+        webDriver.findElement(By.id("create-issue-submit")).clear();
+    }
 }
