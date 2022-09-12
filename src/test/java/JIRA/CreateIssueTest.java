156e0833fb34660637d04cf302204f479ba73919
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2022-09-12 13:36:26 +0200

    createIssue method implemented

--- src/test/java/JIRA/CreateIssueTest.java
+++ src/test/java/JIRA/CreateIssueTest.java
@@ -1,14 +1,17 @@
 package JIRA;
 
 import Main.FileReader;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import dev.failsafe.internal.util.Assert;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.idealized.Javascript;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Random;
 
 public class CreateIssueTest {
     private WebDriver webDriver;
@@ -36,20 +39,49 @@ public class CreateIssueTest {
 
     @Test
     public void createIssue(){
-        // TODO not working yet
+        int randomNumberForId = RandomHelper.generateRandomNumber(100);
+        String issueSummary = "New bug issue " + randomNumberForId;
         user.login();
 
         webDriver.get("https://jira-auto.codecool.metastage.net/browse/MTP-749?filter=-5");
         webDriver.findElement(By.id("create_link")).click();
         RandomHelper.Wait(webDriver);
 
-        webDriver.findElement(By.id("project-field")).clear();
+        webDriver.findElement(By.id("project-field")).click();
+        webDriver.findElement(By.id("project-field")).sendKeys(Keys.BACK_SPACE);
         webDriver.findElement(By.id("project-field")).sendKeys("Main Testing Project (MTP)");
+        webDriver.findElement(By.id("project-field")).sendKeys(Keys.ENTER);
+        RandomHelper.Wait(webDriver);
 
-        webDriver.findElement(By.id("issuetype-field")).clear();
+        webDriver.findElement(By.id("issuetype-field")).click();
+        webDriver.findElement(By.id("issuetype-field")).sendKeys(Keys.BACK_SPACE);
         webDriver.findElement(By.id("issuetype-field")).sendKeys("Bug");
+        webDriver.findElement(By.id("issuetype-field")).sendKeys(Keys.ENTER);
+        RandomHelper.Wait(webDriver);
 
-        webDriver.findElement(By.id("summary")).sendKeys("New bug issue");
-        webDriver.findElement(By.id("create-issue-submit")).clear();
+        webDriver.findElement(By.id("summary")).sendKeys(issueSummary);
+        webDriver.findElement(By.id("create-issue-submit")).click();
+
+        var wait = new WebDriverWait(webDriver, Duration.ofMillis(10000));
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("issue-created-key")));
+        webDriver.findElement(By.className("issue-created-key")).click();
+
+        String searchedIssueSummary = webDriver.findElement(By.id("summary-val")).getText();
+
+        deleteCreatedTestIssue(webDriver);
+
+        Assertions.assertEquals(issueSummary, searchedIssueSummary);
+    }
+
+    private void deleteCreatedTestIssue(WebDriver webDriver){
+        webDriver.findElement(By.id("opsbar-operations_more")).click();
+        JavascriptExecutor jse = (JavascriptExecutor) webDriver;
+
+        WebElement deleteButton = webDriver.findElement(By.id("delete-issue"));
+        jse.executeScript("arguments[0].click()", deleteButton);
+
+        RandomHelper.Wait(webDriver);
+
+        webDriver.findElement(By.id("delete-issue-submit")).click();
     }
 }
--- src/test/java/JIRA/RandomHelper.java
+++ src/test/java/JIRA/RandomHelper.java
@@ -2,6 +2,8 @@ package JIRA;
 
 import org.openqa.selenium.WebDriver;
 
+import java.util.Random;
+
 public class RandomHelper {
     public static void Wait(WebDriver webDriver){
         synchronized (webDriver){
@@ -11,5 +13,11 @@ public class RandomHelper {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    public static int generateRandomNumber(int upperBound){
+        Random random = new Random();
+
+        return random.nextInt(upperBound);
     }
 }
