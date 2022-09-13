7979c59189362bb8a2743564f628815353e33fb0
Szeplaki <szeplaki.eva1@gmail.com>
2022-09-13 14:11:15 +0200

    add test: create issue with all of the project types

--- src/test/java/JIRA/CreateIssueTest.java
+++ src/test/java/JIRA/CreateIssueTest.java
@@ -3,6 +3,9 @@ package JIRA;
 import Main.FileReader;
 import dev.failsafe.internal.util.Assert;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,6 +15,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.Random;
+import java.util.stream.Stream;
 
 public class CreateIssueTest {
     private WebDriver webDriver;
@@ -67,6 +71,7 @@ public class CreateIssueTest {
         webDriver.findElement(By.className("issue-created-key")).click();
 
         String searchedIssueSummary = webDriver.findElement(By.id("summary-val")).getText();
+        System.out.println(searchedIssueSummary);
 
         deleteCreatedTestIssue(webDriver);
 
@@ -100,6 +105,50 @@ public class CreateIssueTest {
         Assertions.assertTrue(elementFound);
         Assertions.assertEquals(expectedMessage,actualMessage);
 
+    }
+
+    @ParameterizedTest
+    @MethodSource("projects")
+    public void createIssueWithAllOfProjectType(String projectName)
+    {
+        int randomNumberForId = RandomHelper.generateRandomNumber(100);
+        String issueSummary = "New project related issue " + randomNumberForId;
+        user.login();
+
+        webDriver.navigate().to("https://jira-auto.codecool.metastage.net/projects/MTP/summary");
+
+        webDriver.findElement(By.id("create_link")).click();
+
+        RandomHelper.Wait(webDriver);
+        webDriver.findElement(By.id("project-field")).click();
+        webDriver.findElement(By.id("project-field")).sendKeys(Keys.BACK_SPACE);
+        webDriver.findElement(By.id("project-field")).sendKeys(projectName);
+        webDriver.findElement(By.id("project-field")).sendKeys(Keys.ENTER);
+        RandomHelper.Wait(webDriver);
+
+        webDriver.findElement(By.xpath("//*[@id=\"create-issue-dialog\"]/header/h2")).click();
+        RandomHelper.Wait(webDriver);
+
+        webDriver.findElement(By.id("summary")).sendKeys(issueSummary);
+        webDriver.findElement(By.id("create-issue-submit")).click();
+
+        var wait = new WebDriverWait(webDriver, Duration.ofMillis(10000));
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("issue-created-key")));
+        webDriver.findElement(By.className("issue-created-key")).click();
+        RandomHelper.Wait(webDriver);
+
+        String actualProjectName = webDriver.findElement(By.id("project-name-val")).getText();
+
+        deleteCreatedTestIssue(webDriver);
+        Assertions.assertTrue(projectName.contains(actualProjectName));
+
+    }
+    private static Stream<Arguments> projects() {
+        return Stream.of(
+                Arguments.arguments("TOUCAN project (TOUCAN)"),
+                Arguments.arguments("JETI project (JETI)"),
+                Arguments.arguments("COALA project (COALA)")
+        );
     }
 
     private void deleteCreatedTestIssue(WebDriver webDriver){
